54750ef7a128ac09f715c68a324e9ae42bc835c3
carlosciid <c.cid.acevedo@gmail.com>
2024-03-28 22:29:11 -0300

    navbar y carusel

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Implementación del 'smooth scroll' 
+document.querySelectorAll('.navbar a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+dsfsdf
