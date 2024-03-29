2b5758d05b248c983d90716537e02c5be909afe1
carlosciid <c.cid.acevedo@gmail.com>
2024-03-29 16:45:38 -0300

    Detalles finales

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,37 @@ document.querySelectorAll('.navbar a[href^="#"]').forEach(anchor => {
         target.scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+
+
+window.addEventListener('scroll', function() {
+    
+    var navbar = document.querySelector('.navbar');
+    var inicioHeight = document.querySelector('#inicio').offsetHeight;
+
+    // reevisa si el scrol se pasa de la seccion inicio
+    if(window.pageYOffset > inicioHeight) {
+        navbar.classList.add('nav-scrolled');
+    } else {
+        navbar.classList.remove('nav-scrolled');
+    }
+});
+
+// mensaje enviar
+document.querySelector('#contacto form').addEventListener('submit', function(e) {
+    
+    e.preventDefault();
+
+    
+    var confirmationMessage = document.querySelector('#confirmationMessage');
+    confirmationMessage.style.display = 'block';
+
+   
+    this.reset();
+
+   
+    setTimeout(function() {
+        confirmationMessage.style.display = 'none';
+    }, 5000); 
+});
+
